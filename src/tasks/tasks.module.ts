064319004748514ab030df72b84401b5102d8738
Jing Xiang <jxsim.2013@gmail.com>
2022-03-12 16:54:36 +0800

    add authentication and authorisation

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TasksRepository } from './tasks.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TasksRepository]),
+    AuthModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
